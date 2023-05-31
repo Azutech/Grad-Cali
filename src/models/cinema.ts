9b7024346e27c8539051a47749a7beebdc77e4b6
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-05-31 15:10:38 +0100

    chore(): Model for cinema collections for cinema are set-up [MODELS ]

--- src/models/cinema.ts
+++ src/models/cinema.ts
@@ -1,10 +1,19 @@
-import {model, Schema} from 'mongoose'
-
+import { model, Schema } from 'mongoose'
 
 const cinemaSchema = new Schema(
     {
+        movie: {
+            type: String,
+            required: [true, 'movie is required'],
+        },
+        capacity: {
+            type: Number,
+        },
 
+        availableSeats: {
+            type: Number,
+        },
     },
-    {timestamps: true}
+    { timestamps: true }
 )
 export const Cinema = model('cinema', cinemaSchema)
